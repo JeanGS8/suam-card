16593a98a0c6cf7c335775d970532440e5bb75c0
JeanGS8 <jean118877@hotmail.com>
2024-03-28 18:40:15 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cardPerfil',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
